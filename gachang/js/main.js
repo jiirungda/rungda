1f6d33a14eb056f0e0f73acda02295429f8bc206
jiirungda <jii020116@gmail.com>
2025-07-02 15:23:55 +0900

    250702

--- gachang/js/main.js
+++ gachang/js/main.js
@@ -1,14 +1,23 @@
 $(document).ready(function(){
-
-    const service_swiper = new Swiper('.service .swiper', { /* 팝업을 감싼는 요소의 class명 */
-        slidesPerView: 'auto', /* auto css에서 준대로 들어감*/
+	const service_swiper = new Swiper('.service .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
         spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
         breakpoints: {
-            768: {    /* 640px 이상일때 적용 */
+            768: {    /* 768px 이상일때 적용 */
+                slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+            1024: {    /* 1024px 이상일때 적용 */
+                slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
                 spaceBetween: 24,
             },
         },
-        //loop: true,
-    });
+        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: false,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        //autoplay: {  /* 팝업 자동 실행 */
+        //	delay: 2500,
+        //	disableOnInteraction: true,
+        },
+    );
 
-})
+});
