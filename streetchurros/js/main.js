c8e1cf8684ae2741cadb12ff38a82566fb3083ae
jiirungda <jii020116@gmail.com>
2025-07-14 18:00:18 +0900

    250714

--- streetchurros/js/main.js
+++ streetchurros/js/main.js
@@ -26,7 +26,30 @@ $(document).ready(function(){
         visual_swiper.autoplay.start();  /* 재생 기능 */
         $(this).hide() //재생버튼 자신은 숨김
         $('.visual .btn_wrap button.btn_stop').show() //정지버튼이 나타남
-    })
+    });
 
     /********************visual swiper : 종료*********************/
-});
+    /********************snsarea swiper : 시작*********************/
+    
+    const snsarea_swiper = new Swiper('.snsarea .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            768: {    /* 640px 이상일때 적용 */
+                spaceBetween: 24,
+                centeredSlides: true
+            },
+        },
+        centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        autoplay: {  /* 팝업 자동 실행 */
+		delay: 6000,
+		disableOnInteraction: true,
+	    },
+    });
+	
+
+    /********************snsarea swiper : 종료*********************/
+
+
+})
