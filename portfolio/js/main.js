ea10dc7c6edc6fac9239e8461fe987c7a65379af
jiirungda <jii020116@gmail.com>
2025-07-22 18:00:11 +0900

    .

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,21 +1,33 @@
 $(document).ready(function(){
 
+	const myFullpage = new fullpage('#fullpage', {  /* html에서 페이지 전체를 감싸는 요소 */
 
-    const myFullpage = new fullpage('#fullpage', {   /* html에서 페이지 전체를 감싸는 요소 */ 
+		lockAnchors: false,
+		anchors: ['main', 'profile', 'project01', 'project02', 'thankyou'], /* href="#link1" 이렇게 코딩하면 해당 링크명으로 이동 */
 
-		Navigation: true, /* 오른쪽에 각 페이지의 paging */ 
-		NavigationPosition: 'right', /* 위치 */ 
-		NavigationTooltips: ['첫번째', '두번째', '세번째', '네번째'], /*툴팁 */ 
-		showActiveTooltip: true, /* 현재 활성화된 페이지의 툴팁에 특정 클래스 */ 
+		autoScrolling:true, /* 한페이지씩 스크롤 */
+		scrollHorizontally: true,
+
+		verticalCentered: true, /* 컨텐츠 요소 위아래 가운데 */
 		
-		lockAnchors: true, 앵커: ['link1', 'link2', 'link3', 'link4'], /* href="#link1" 방식으로 코딩하면 해당 링크명으로 이동 */ 
+		scrollOverflow: false, /* 컨텐츠가 넘쳐도 스크롤 금지 */
 
-		autoScrolling:true, /* 한페이지씩 스크롤 */ 
-		scrollHorizontally: true, VerticalCentered: true, /* 컨텐더 위키는 중앙에 */ 
-		
-		scrollOverflow: false, /* 컨텐츠가도 이동 금지 */ 
+		afterLoad: function(origin, destination, direction, trigger){
+			if(destination.index == 0){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
+				//console.log('1번째 슬라이드가 로딩 되었을때');
+				
+			}else if(destination.index == 1){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
+				//console.log('2번째 슬라이드가 로딩 되었을때');
+				
+			}else if(destination.index == 2){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
+				//console.log('3번째 슬라이드가 로딩 되었을때');
+				
+			}else if(destination.index == 3){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
+				//console.log('4번째 슬라이드가 로딩 되었을때');
+				
+			}
+		},
 
-		afterLoad: function(origin, 목적지, 방향, 트리거){ if(destination.index == 2){ /* 색인가 2면 슬라이드는 슬라이드입니다. index 수는 0/1/2/3 */ 
-				console.log('3번째 슬라이드가 실행된 날'); } }, ResponseWidth: 640 /* fullpage를 적용하면 예외적인 모바일 크기 */ 
-	});
+	}); //fullpage
+    
 })
