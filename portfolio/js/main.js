c5a8820c03102614afa00994162e1f962e858c65
jiirungda <jii020116@gmail.com>
2025-07-23 15:38:44 +0900

    .

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -29,5 +29,16 @@ $(document).ready(function(){
 		},
 
 	}); //fullpage
+
+	$(window).on('scroll mousemove', function(e){  /* html cursor가 마우스 포인터를 따라다니게 하는 값 */
+		$('.cursor').css('left', e.pageX + 'px');
+		$('.cursor').css('top', e.pageY + 'px');
+	});
+	$('.project01 .wrapper .photo a').hover(function(){ /* 특정한 요소에 마우스를 올렸을때만 on 클래스 주기 */
+		$('.cursor').toggleClass('on');
+	});
+	$('.project02 .wrapper .photo a').hover(function(){ /* 특정한 요소에 마우스를 올렸을때만 on 클래스 주기 */
+		$('.cursor').toggleClass('on');
+	});
     
 })
